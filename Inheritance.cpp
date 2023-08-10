3bf61d59df299e380c2a3a194e738a1672d6521b
Khushi Mishra <104066423+kmishraa@users.noreply.github.com>
2023-08-11 00:02:56 +0530

    Create Inheritance.cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Person
+{
+    protected:
+        string name;
+        int id;
+    public:
+        Person(string n, int i){
+            name = n;
+            id = i;
+        }
+};
+
+class Student : public Person
+{
+    private:
+        int marks;
+    public:
+        Student(string n, int i, int m): Person(n, i),marks(m){
+           
+        }void print(){
+            cout << name << " " << id << " " << marks << endl;
+        };
+};
+  
+
+int main()
+{
+
+Student stu("Rahul", 1001, 88);
+stu.print();
+}
